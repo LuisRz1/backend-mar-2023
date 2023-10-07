8d48fe7a259223c4de5857ba7d09dabe409941e1
LuisRz1 <langulor1@upao.edu.pe>
2023-10-07 04:03:02 -0500

    first commit

---
+++ src/test/java/pe/edu/upao/backendmar/BackendMarApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.backendmar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
